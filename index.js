1890e4e9cd16974ed754e7c374eab416ddb1e7ce
Carlotta Chen <carlottachen92@gmail.com>
2021-11-11 10:29:44 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//Carlotta
+
+//console.log(`hello world`);
+
+let backpack = [];
+
+backpack.push('sword', 'shield', 'food');
+
+console.log(backpack);
+
+//took first item from array, assigned to sword array
+//let sword = backpack.shift();
+
+//start at index 1 and remove one item
+//added to new array called sword
+let sword = backpack.splice(1, 1);
+console.log(backpack, sword);
+
+//insert item in sword back into backpack
+//item will not be removed from sword array
+backpack.unshift(sword[0]);
+console.log(backpack, sword);
+
+//add a fur coat to backpack at end of array
+let furCoat = 'fur coat';
+backpack.push(furCoat);
+console.log(backpack, sword);
+
+//will remove last item, not stored anywhere else
+backpack.pop();
+console.log(backpack, sword);
+
+//see how many items inside backpack array
+let itemCount = backpack.length;
+console.log(itemCount);
+
+//add more items to end of backpack array
+backpack.push('flint', 'blanket', 'knife', 'toothbrush');
+itemCount = backpack.length;
+console.log(backpack);
+
+let backpack2 = backpack.splice(2, 3);
+console.log(backpack, backpack2);
+
+
+
